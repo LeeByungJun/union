39f3c8d7cdabb333c4ab010229d33353bf02dd01
BJLee <BJLee@DESKTOP-7U80AS0>
2020-06-22 13:24:22 +0900

    2020.06.22.LBJ - Lombok jar 설정에 따른 getter, setter 제거

--- unionProject/src/main/java/com/example/demo/vo/SmsVo.java
+++ unionProject/src/main/java/com/example/demo/vo/SmsVo.java
@@ -10,29 +10,5 @@ public class SmsVo {
 	private String sendNo;
 	private String userId;
 	private ArrayList<RecipientNo> recipientList = new ArrayList<RecipientNo>();
-	public String getBody() {
-		return body;
-	}
-	public void setBody(String body) {
-		this.body = body;
-	}
-	public String getSendNo() {
-		return sendNo;
-	}
-	public void setSendNo(String sendNo) {
-		this.sendNo = sendNo;
-	}
-	public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public ArrayList<RecipientNo> getRecipientList() {
-		return recipientList;
-	}
-	public void setRecipientList(ArrayList<RecipientNo> recipientList) {
-		this.recipientList = recipientList;
-	}
 	
 }
